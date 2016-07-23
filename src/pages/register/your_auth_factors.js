d5856a13af964cc603cad04d08952aa92d876377
mark.middleton <markymiddleton@gmail.com>
2016-07-23 09:26:36 +0100

    Tidy up your_auth_factors page

--- src/pages/register/your_auth_factors.js
+++ src/pages/register/your_auth_factors.js
@@ -36,49 +36,47 @@ export default connect((state) => state) (
 
             let session = this.props.session.registration;
 
-            let handlers = {
-                google_authenticator: () => {
-                    if ( session.google_authenticator ) {
-                        return(
-                            <span className="second_factor_already_setup">Google authenticator - [Already setup]</span>
-                        )
-                    }
-                    else return(
-                        <label className="block-label" for="radio-1" key="radio-1">
-                            <input ref="ga" id="radio-1" type="radio" name="radio-group"/>Google authenticator
-                        </label>
-                    )
-                },
-                device_fingerprint: () => {
-                    if ( session.device_fingerprint ) {
-                        return(
-                            <span className="second_factor_already_setup">Device fingerprint - [Already setup]</span>
-                        )
-                    }
-                    else return(
-                        <label className="block-label" for="radio-2" key="radio-2">
-                            <input ref="df" id="radio-2" type="radio" name="radio-group"/>Device fingerprint
-                        </label>
-                    )
-                },
-                u2f_key: () => {
-                    if ( session.u2f_key ) {
-                        return(
-                            <span className="second_factor_already_setup">U2F Key - [Already setup]</span>
-                        )
-                    }
-                    else return(
-                        <label className="block-label" for="radio-3" key="radio-3">
-                            <input ref="u2f" id="radio-3" type="radio" name="radio-group"/>U2F Key
-                        </label>
-                    )
-                }
+            let res = [];
 
-            };
+            if ( session.google_authenticator ) {
+                res.push(<div><span className="second_factor_already_setup">Google authenticator - setup</span><br/></div>)
+            }
+
+            if ( session.device_fingerprint ) {
+                res.push(<div><span className="second_factor_already_setup">Device fingerprint - setup</span><br/></div>)
+            }
+
+            if ( session.u2f_key ) {
+                res.push(<div><span className="second_factor_already_setup">U2F key - setup</span><br/></div>)
+            }
 
 
+            if ( !session.google_authenticator ) {
+                res.push(
+                    <label className="block-label" for="radio-1" key="radio-1">
+                        <input ref="ga" id="radio-1" type="radio" name="radio-group"/>Google authenticator
+                    </label>
+                )
+            }
 
-            return Object.keys(handlers).map( (f) => handlers[f]());
+            if ( !session.device_fingerprint ) {
+                res.push(
+                    <label className="block-label" for="radio-2" key="radio-2">
+                        <input ref="df" id="radio-2" type="radio" name="radio-group"/>Device fingerprint
+                    </label>
+                )
+            }
+
+            if ( !session.u2f_key ) {
+                res.push(
+                    <label className="block-label" for="radio-3" key="radio-3">
+                        <input ref="u2f" id="radio-3" type="radio" name="radio-group"/>U2F Key
+                    </label>
+                )
+            }
+
+            return res;
+
         }
 
         render() {
