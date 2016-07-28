2d9a73259f164d3cde309e266100c6bb354f6332
mark.middleton <markymiddleton@gmail.com>
2016-07-28 19:00:06 +0100

    Added cucumber tests

--- test/features/spacegov.feature
+++ test/features/spacegov.feature
@@ -1,19 +1,44 @@
 Feature: Spacegov feature
   As a user of Spacegov I want to authenticate via government gateway
 
-Scenario: Level 1
-  Given I'm enroled onto Spacegov as average@joe.com
+
+Background:
+  Given I have registered for spacegov with name: "Mark Middleton", email: "markymiddleton@gmail.com", password: "password"
+
+Scenario: Level 1 and Level 2 trust
+  When I log into spacegov with email: "markymiddleton@gmail.com", password: "password"
   Then I should see:
-   | Service trusts you to level 1 |
-  When I click "Apply for a grant to clean"
-  Then I should be on the "Two step verification" page
+    | Service trusts you to level 1 |
+  And I click "Apply for a grant to clean"
+  And I should see:
+    | Spacegov would like you to use a second authentication factor |
   And I don't use two step verification
   Then I should see:
     |As you were unable to authenticate with a second factor, you will need to visit your local Spacegov office.|
-
-Scenario: Level 2
-  Given I'm enroled onto Spacegov as average@joe.com
+  And I click "Continue"
+  Then I should see:
+    | Service trusts you to level 1 |
   When I click "Apply for a grant to clean"
-  Then I should be on the "Two step verification" page
   And I use Google authenticator
   Then I should be on the "Apply for cleaning grant" page
+  And I click "Apply"
+  Then I should be on the "Grant appliction submitted" page
+  And I click "Continue"
+  Then I should see:
+    | Service trusts you to level 2 |
+
+
+Scenario: Break trust
+  When helpdesk agent searches for "markymiddleton@gmail.com"
+  And helpdesk agent unable to prove identity and breaks trust
+  And I log into spacegov with email: "markymiddleton@gmail.com" and reset password
+  And I should be on the "We need to check that it really is you." page
+
+
+
+Scenario: Manage my account
+  When I log into spacegov with email: "markymiddleton@gmail.com", password: "password"
+  And I click "View my details"
+  And I click hint link "Manage my account"
+  And I click "Manage my account"
+  Then I should be on the "Credential Management" page
--- test/features/step_definitions/steps.rb
+++ test/features/step_definitions/steps.rb
@@ -22,16 +22,46 @@ And /^I don't use two step verification$/ do
   click_link "Continue"
 end
 
+And /^I choose "(.*)"$/ do |choice|
+  choose choice
+end
+
 And /^I use Google authenticator$/ do
   choose("Google authenticator")
   click_link "Continue"
+
   code = find(:css, '.form-hint').text
-  fill_in "code", with: code[0]
+  if code.is_a? Array
+    code=code[0]
+  end
+
+  fill_in "code", with: code
   click_link "Continue"
 end
 
+And /^I enter: (.*)$/ do |data|
+  d = eval(data)
+  d.each do |name, val|
+    fill_in name, with: val
+  end
+end
 
 
+And /^I'm registering for Spacegov$/ do
+  visit "https://localhost:3000"
+  click_link "Spacegov"
+  click_link "Sign into Spacegov"
+  click_hint_link "Create a new account"
+  click_link "Register a new account"
+end
+
+And /^I click hint link "(.*)"$/ do |link|
+  click_hint_link link
+end
+
+def click_hint_link name
+  find("span", :text => Regexp.new(name)).click
+end
 
 
 And /^I wait$/ do
@@ -75,3 +105,128 @@ end
 And(/^I click "(.*)"$/) do |link|
   click_link link
 end
+
+
+And /^I have registered for spacegov with name: "(.*)", email: "(.*)", password: "(.*)"$/ do |name, email, password|
+  register_for_spacegov email, name, password
+end
+
+
+And /^I log into spacegov with email: "(.*)", password: "(.*)"$/ do |email,password|
+  click_link "Spacegov"
+  click_link "Sign into Spacegov"
+  fill_in "email", with: email
+  fill_in "password", with: password
+  click_link "Continue"
+  click_link "Continue"
+end
+
+And /^I log into spacegov with email: "(.*)" and reset password$/ do |email|
+  click_link "Spacegov"
+  click_link "Sign into Spacegov"
+  fill_in "email", with: email
+  fill_in "password", with: @new_password
+  click_link "Continue"
+  click_link "Continue"
+end
+
+
+And /^helpdesk agent searches for "(.*)"$/ do |email|
+  search_helpdesk_for email
+end
+
+And /^helpdesk agent unable to prove identity and breaks trust$/ do
+  click_hint_link "Customer unable to prove their identity"
+
+  click_link "manage_account_and_break_trust"
+  # manage account
+  click_link "Reset password"
+  # reset password
+  @new_password = find(:css, '.password_box').text
+  click_link "Continue"
+  click_link "Save changes and break trust"
+  click_link "Sign out"
+end
+
+
+
+def register_for_spacegov email,name,password
+  visit "https://localhost:3000"
+  click_link "Spacegov"
+  click_link "Sign into Spacegov"
+  click_hint_link "Create a new account"
+  click_link "Register a new account"
+  # What's your name page
+  fill_in "name", with: name
+  click_link "Continue"
+  # What is your email address
+  fill_in "email", with: email
+  fill_in "confirm_email", with: email
+  click_link "Continue"
+  #Confirm email code?
+  fill_in "code", with: "1234"
+  click_link "Continue"
+  #Set your password
+  fill_in "password", with: password
+  fill_in "password2", with: password
+  click_link "Continue"
+  # Your authentication factors
+  setup_google_authenticator
+  choose "I'm done"
+  click_link "Continue"
+  # Your government gateway account has been created
+  click_link "Save and Sign in"
+  # enrolment
+  fill_in "org", with: "Zaltek"
+  fill_in "mission", with: "To boldly go, where no man has gone before"
+  click_link "Enrol for Spacegov"
+  click_link "Continue"
+  click_link "Sign out"
+end
+
+def setup_google_authenticator
+  choose("Google authenticator")
+  click_link "Continue"
+
+  code = find(:css, '.form-hint').text
+  if code.is_a? Array
+    code=code[0]
+  end
+
+  fill_in "code", with: code
+  click_link "Continue"
+end
+
+
+def search_helpdesk_for email
+  click_link "Helpdesk"
+  click_link "Sign into Helpdesk"
+  # sign in
+  fill_in "email", with: "average@joe.com"
+  fill_in "password", with: "password"
+  click_link "Continue"
+  # two step verification
+  click_link "Continue"
+  # Welcome back
+  click_link "Search"
+  # Search for user
+  fill_in "email", with: email
+  click_link "Search"
+  # results
+  click_link "Prove Identity"
+end
+
+
+
+#   And I should be on the "Setup two step verification" page
+#   And I use Google authenticator
+#   And I should be on the "Setup two step verification" page
+#   And I choose "I'm done"
+#   And I click "Continue"
+#   And I should be on the "Your government gateway account has been created" page
+#   And I should see:
+#   |	markymiddleton@gmail.com  |
+#   |	Mark Middleton  |
+#   |	Password, Google authenticator  |
+
+
